2b69b404bc28362853f5b97f40536786e64a73f2
Jorge Olivé <j.olive.rodriguez@gmail.com>
2022-01-23 01:50:45 +0100

    cards

--- components/PostPreview.vue
+++ components/PostPreview.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="preview-card">
-      <span class="date">{{formatDate(post.createdAt)}}</span>
+    
+    <div class="date">
+      <div style="display: flex;">
+        <span style=" flex: 1 4 50%; text-align: left; padding-left: 10px;">{{formatDate(post.createdAt)}}</span>
+        <div class="tags">
+          <div class="tag" v-for="tag in tags" :key=tag>
+          <span>{{ tag }}</span>
+      </div>
+    </div>
+      </div>
+    </div>
+    <NuxtLink :to="{ name: 'posts-slug', params: { slug: post.slug } }">
       <h2 style = "flex: 0 1 25%; justify-content: center;">{{ post.title }}</h2>
-      <nuxt-img class="img" v-if="post.image != null" v-bind:src="post.image" 
-        width="400"
-        height="250"/>
-      <span class="description">{{ post.description }}</span>
+    </NuxtLink>
+    <nuxt-img class="img" v-if="post.image != null" v-bind:src="post.image" 
+      width="400"
+      height="250"/>
+    <span class="description">{{ post.description }}</span>
   </div>
 </template>
 
@@ -20,11 +32,28 @@ export default {
       const options = { year: 'numeric', month: 'long', day: 'numeric' }
       return new Date(date).toLocaleDateString('en', options)
     }
+  },
+  computed: {
+    tags: function () {
+      return this.post.tags?.split(";") ?? [];
+    }
   }
 }
 </script>
 
 <style>
+a {
+  text-decoration: none;
+}
+
+a:visited {
+  color: black;
+}
+
+a:hover {
+  color: blue;
+}
+
 .preview-card {
   display: flex;
   flex-direction: column;
@@ -40,14 +69,14 @@ export default {
 }
 
 .img {
-  flex: 0 0 40%;
+  flex: 0 0 35%;
   display: block;
   margin-left: auto;
   margin-right: auto;
 }
 
 h2 {
-  flex: 0 0 30%;
+  flex: 0 0 25%;
 }
 
 .description {
@@ -56,6 +85,21 @@ h2 {
 
 .date {
   flex: 0 0 5%;
+}
+
+.tags {
+  flex: 1 4 50%;
+  padding: 3px;
+  margin: 3px;
+}
+
+.tag {
+  padding: 3px;
+  margin: 3px;
+  display: inline;
+  background-color: rgb(223 219 219);
+  border-radius: 5px;
+  font-size: 15px;
 }
 
 </style>
--- content/posts/my-second-post.md
+++ content/posts/my-second-post.md
@@ -3,6 +3,7 @@ title: "Implementing a rate limiter: Leaky bucket algorithm"
 description: DDD Community will probably say so.
 custom_field: this is my custom field
 image: 'datacenter.jpg'
+tags: algorithms; systems-design
 ---
 
 ### ¡hello there
