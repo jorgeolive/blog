228bce5c9779ea6bd258fb2cb4dbe7fa690f4ad9
Jorge Olivé <j.olive.rodriguez@gmail.com>
2022-02-09 23:08:17 +0100

    open graph

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -32,7 +32,11 @@ export default {
           hid: 'description',
           name: this.post.title,
           content: this.post.htmlMetadata
-        }
+        },
+        { hid: 'og:title', property: 'og:title', content: this.post.title },
+        { hid: 'og:description', property: 'og:description', content: this.post.htmlMetadata },
+        { hid: 'twitter:title', name: 'twitter:title', content: this.post.title },
+        { hid: 'twitter:description', name: 'twitter:description', content: this.post.htmlMetadata }
       ],
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
     }
